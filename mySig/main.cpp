f426d0f396ece310f52f1f689fe20717a4b71eb4
wanbess <3520644205@qq.com>
2023-06-10 23:33:28 +0800

    automated destroy all connection by derived from a base class

--- mySig/main.cpp
+++ mySig/main.cpp
@@ -2,7 +2,7 @@
 #include"siglink.h"
 #include<functional>
 #include<iostream>
-struct A {
+struct A :mysig ::Object{
 	void funcA(int a, int b) {
 		std::cout <<"class in:" << a << " " << b << std::endl;
 	}
@@ -12,15 +12,17 @@ void func_free(){}
 int main() {
 	using namespace mysig;
 	A a;
+	
 	Base_Signal<int, int> base;
 	connect(&base, &A::funcA, &a);
 	connect(&base, [](int a,int b) {std::cout << "lambda in:" << a << " " << b << std::endl; });
 	//base.emit(2, 3);
 	//disconnect(&base, &A::funcA, &a);
+	base.emit(2, 3);
 	Base_Signal<int, int> base2;
 	connect(&base2, &A::funcA, &a);
-	base.emit(2, 3);
-	disconnect_all(&A::funcA, &a);
+	
+	//disconnect_all(&A::funcA, &a);
 	printf("after disconnect_all\n");
 	base.emit(2, 3);
 }
